83f7d60c128357b606cd2a2e857e0ab8465dceb7
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-08-09 14:00:55 +0300

    create seed data

---
+++ bin/seeds.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const User = require("../models/User.model");
+const Recipe = require("../models/Recipe.model");
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/recipies-site";
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+
+const users = require("./users-seed-data");
+const recipes = require("./recipes-seed-data");
+
+User.create(users)
+  .then((usersFromDB) => {
+    console.log(`Created ${usersFromDB.length} users`);
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch((err) => console.log(`An error occurred while uploading users to the DB: ${err}`));
+
+Recipe.create(recipes)
+  .then((recipesFromDB) => {
+    console.log(`Created ${recipesFromDB.length} recipes`);
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch((err) => console.log(`An error occurred while uploading recipes to the DB: ${err}`));
---
+++ bin/users-seed-data.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+        name: 'Julie',
+        email: 'julie@julie.com',
+        username: 'KitchenJulie',
+        recipes: [
+            {
+                title: 'Pumpkin And Kale Curry',
+                content: "Wash vegetables. Remove pumpkin peel. Discard of kale stems and tear the leaves. Grate peeled ginger, squeeze garlic cloves with garlic press. Cut pumpkin into bite sized pieces.Heat a deep heavy bottomed pan or dutch oven, add oil. Add garlic with ginger, fry for a couple of minutes then add all of the spices. Heat just for a moment, until fragrant, then pour in coconut milk.",
+                image: 'https://images.squarespace-cdn.com/content/v1/589cc02edb29d6783372fb86/1603821030425-2HPGQB22ZR9FDVSIJE8B/pumpkin_and_kale_curry-SMALLER-1.jpg?format=500w'
+        
+            },
+            {
+                title: 'Emerald Dal',
+                content: "This Lentil Dal with Spinach Sauce is one of the most delicious, soul-satisfying plant-based, Indian meals! This version is fragrant, flavorful and packed with nutrients- think of this like Saag Paneer, but substituting black lentils instead of the cheese! Super tasty and healthy.",
+                image: 'https://www.feastingathome.com/wp-content/uploads/2020/06/Lentil-Dal-15-768x1152.jpg'
+            }
+        ] 
+    }
+]
