da13ae8a9cc874afd0b0364e667eee36bc11b43d
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-08-12 12:57:58 +0300

    add fucntionality, user can edit/delete profile

--- bin/recipes-seed-data.js
+++ bin/recipes-seed-data.js
@@ -6,7 +6,7 @@ const recipes = [
     title: "Pumpkin And Kale Curry",
     content:
       "Wash vegetables. Remove pumpkin peel. Discard of kale stems and tear the leaves. Grate peeled ginger, squeeze garlic cloves with garlic press. Cut pumpkin into bite sized pieces.Heat a deep heavy bottomed pan or dutch oven, add oil. Add garlic with ginger, fry for a couple of minutes then add all of the spices. Heat just for a moment, until fragrant, then pour in coconut milk.",
-    image:
+    imageURL:
       "https://images.squarespace-cdn.com/content/v1/589cc02edb29d6783372fb86/1603821030425-2HPGQB22ZR9FDVSIJE8B/pumpkin_and_kale_curry-SMALLER-1.jpg?format=500w",
   },
 ];
