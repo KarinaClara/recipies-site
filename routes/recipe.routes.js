ead0ceb01935d566872779c660ae94c0eb7896fe
claradrojas <drc.clara@gmail.com>
2021-08-13 08:27:54 +0200

    styling for recipes and detail pages

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -15,7 +15,7 @@ const fileUploader = require('../config/cloudinary.config')
 router.get('/recipes', (req, res, next) => {
     Recipe.find()
         .then(allRecipes => {
-            //console.log('ALL RECIPES HELLO ', allRecipes)
+
             res.render('recipe/recipes', { recipes: allRecipes })
         })
         .catch(error => {
@@ -27,29 +27,30 @@ router.get('/recipes', (req, res, next) => {
 
 // CREATE
 router.get('/recipe/create', isLoggedIn, (req, res, next) => {
+
     res.render("recipe/new-recipe")
 })
 
 router.post('/recipe/create', fileUploader.single('recipe-cover-image'), isLoggedIn, (req, res, next) => {
-    const { author , title, content } = req.body; 
-    //console.log('CONSOLE LOG REQ BODY: ', req.body.author)
-     
-    const imageURL = req.file.path 
+
+    const { author, title, content } = req.body;
+
+    const imageURL = req.file?.path;
+
 
     Recipe.create({ author, title, content, imageURL })
-    .then((newRecipe) => {
-        //console.log('THERE IS NEW REC', newRecipe)
-        //console.log('AUTHOOOR', author)
-        return User.findByIdAndUpdate(author, { $push: { recipes: newRecipe._id, newRecipe } });
-      })
-        .then((newRecipe)=> {
-           // console.log('THERE IS NEW REC 2222', newRecipe)
+        .then((newRecipe) => {
+
+            return User.findByIdAndUpdate(author, { $push: { recipes: newRecipe._id, newRecipe } });
+        })
+        .then((newRecipe) => {
+
             res.redirect('/recipes')
         })
         .catch(error => {
             console.log('Error while saving recipe', error)
             next(error)
-          
+
         })
 })
 
@@ -70,62 +71,61 @@ router.get('/recipe/:recipeId/edit', isLoggedIn, (req, res, next) => {
 
 router.post('/recipe/:recipeId/edit', fileUploader.single('recipe-cover-image'), isLoggedIn, (req, res, next) => {
     const { recipeId } = req.params
-    const {author, title, content } = req.body 
-    console.log('HELLO:', req.body  )
+    const { author, title, content } = req.body
 
     //to display latest image if user doesn't upload any 
-    const recipeData = { 
+    const recipeData = {
         author,
-        title, 
-        content, 
+        title,
+        content,
     }
 
-    if (req.file){
-        recipeData.imageURL= req.file.path
+    if (req.file) {
+        recipeData.imageURL = req.file.path
     }
 
-    Recipe.findByIdAndUpdate(recipeId, recipeData, {new:true})
-    .then(updatedRecipe => {
-        res.redirect(`/recipe/${updatedRecipe._id}`)
-      
-    })
-    .catch(error => {
-        console.log('There was an error while updating data', error)
-        next(error)
-    })
+    Recipe.findByIdAndUpdate(recipeId, recipeData, { new: true })
+        .then(updatedRecipe => {
+            res.redirect(`/recipe/${updatedRecipe._id}`)
+
+        })
+        .catch(error => {
+            console.log('There was an error while updating data', error)
+            next(error)
+        })
 })
 
 
-          
+
 //DETAILS PAGE
-router.get('/recipe/:recipeId', (req, res, next)=> {
+router.get('/recipe/:recipeId', (req, res, next) => {
     const recipeId = req.params.recipeId
 
     Recipe.findById(recipeId)
-    .populate('author')
-    .then(recipeDetails => {
-        res.render('recipe/recipe-details', {recipe: recipeDetails})
-    })
-    .catch(error => {
-        console.log('There was an error while displaying the details', error)
-        next(error)
-    })
+        .populate('author')
+        .then(recipeDetails => {
+            res.render('recipe/recipe-details', { recipe: recipeDetails })
+        })
+        .catch(error => {
+            console.log('There was an error while displaying the details', error)
+            next(error)
+        })
 })
 
 // DELETE
 router.post('/recipe/:recipeId/delete', isLoggedIn, (req, res, next) => {
-    const {recipeId} = req.params
+    const { recipeId } = req.params
     console.log('CONSOLEE', recipeId)
     Recipe.findByIdAndDelete(recipeId)
-    .then( (recipeId) => {
-        console.log('CONSOLEE 22', recipeId)
-        res.redirect('/recipes')
-    })
-    .catch(error => {
-        console.log('Error while deleting recipe', error)
-        next(error)
-        
-    })
+        .then((recipeId) => {
+            console.log('CONSOLEE 22', recipeId)
+            res.redirect('/recipes')
+        })
+        .catch(error => {
+            console.log('Error while deleting recipe', error)
+            next(error)
+
+        })
 
 })
 
