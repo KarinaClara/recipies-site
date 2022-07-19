1120e16cd2f0bb1f7fa8376e0fea6747aab86965
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2022-07-19 10:20:44 +0300

    update isAuthor middleware

--- middleware/isAuthor.js
+++ middleware/isAuthor.js
@@ -4,10 +4,10 @@ module.exports = (req, res, next) => {
 
     Recipe.findById(req.params.recipeId)
         .then(recipe => {
-            if (req.session.user._id == recipe.author) {
+            if (req.session.user && req.session.user._id == recipe.author) {
                 res.locals.isTheAuthor = true;
             }
             next()
         })
-
+        .catch(err => console.log('error looking up recipe', err ))
   };
